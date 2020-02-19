a0c16c2913d7c7f8ea43d8812ec795328825a39a
deek88 <deek.n8@gmail.com>
2020-02-19 22:36:39 +0100

    Refractoring Code: Removing redundant code in function load_data

--- bikeshare.py
+++ bikeshare.py
@@ -72,7 +72,6 @@ def load_data(city, month, day):
     df['hour'] = df['Start Time'].dt.hour
     #filter according to month
     if month != 'all':
-        MONTHS = ['january', 'february', 'march', 'april', 'may', 'june']
         month = MONTHS.index(month) + 1
         #filter by MONTHS
         df = df[df['month'] == month]
